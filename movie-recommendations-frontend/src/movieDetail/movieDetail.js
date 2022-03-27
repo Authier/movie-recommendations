4264209edbdbdf1180890cd5be71a16fd42d11f4
Jacob Authier <authierjacob@gmail.com>
2022-03-27 09:43:53 -0500

    Fourth Commit

--- movie-recommendations-frontend/src/movieDetail/movieDetail.js
+++ movie-recommendations-frontend/src/movieDetail/movieDetail.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './movieDetail.css';
+import { BsFillStarFill } from 'react-icons/bs';
+import { BiArrowBack } from 'react-icons/bi';
 
 // key={use_id}
 // id={use_id}
@@ -19,13 +21,33 @@ import './movieDetail.css';
 export default function MovieDetail (props) {
     console.log(props.movieDetails)
     return (
-        <div onClick={props.close} className="movie-detail-container">
+        <div className="movie-detail-container">
+            <div className="closing-button"  onClick={props.close} >
+                <BiArrowBack size={70} 
+                id="closing-button" 
+                />
+            </div>
             <div className="movie-detail-inner-container">
                 <img id="movie-detail-poster" src={props.movieDetails.poster_path} />
                 <div className="movie-detail-info">
                     <h1 id="movie-detail-title">{props.movieDetails.title}</h1>
                     <div className="movie-detail-description">
-                        <p>
+                        <div className="movie-detail-d2"> 
+                            <div className="movie-detail-d2-children">
+                                {props.movieDetails.release_date}
+                            </div>
+                            <div className="movie-detail-d2-children">
+                                
+                            </div>
+                            <div className="movie-detail-d2-children">
+                                {props.movieDetails.vote_average}
+                                &nbsp;
+                                <BsFillStarFill color="gold"/>
+                                &nbsp;
+                                ({props.movieDetails.vote_count})
+                            </div>
+                        </div>
+                        <p className="movie-detail-overview">
                             {props.movieDetails.overview}
                         </p>
                     </div>
